e67227727636433388a74e84e4fde067c2dcdf11
Amirhosein Feiz <amir1380feiz@gmail.com>
2025-04-17 17:36:05 +0330

    [Add] Add source package.

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+from src.config_reader import *
+from src.kinematics import *
+from src.logger import *
+from src.preprocess import *
+from src.preprocess_utils import *
+from src.smpl_model import *
